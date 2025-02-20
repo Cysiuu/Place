b0c1dddd4a68d67cfaeec509b62f4bc1238b535d
Cysiuu <cysiuu021@protonmail.com>
2025-02-20 13:40:08 +0100

    Add layout component

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+</head>
+<body>
+<nav>
+    <a href="{{ route('home') }}">Home</a>
+    <a href="{{ route('explore') }}">Explore</a>
+    <a href="{{ route('collections') }}">Collections</a>
+    <a href="{{ route('top-places') }}">Top places</a>
+    <a href="{{ route('near-me') }}">Near me</a>
+</nav>
+</body>
+</html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,3 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Home</title>
-</head>
-<body>
-    <nav>
-        <a href="{{ route('home') }}">Home</a>
-        <a href="{{ route('explore') }}">Explore</a>
-        <a href="{{ route('collections') }}">Collections</a>
-        <a href="{{ route('top-places') }}">Top places</a>
-        <a href="{{ route('near-me') }}">Near me</a>
-    </nav>
-</body>
-</html>
+<x-layout>
+
+</x-layout>
