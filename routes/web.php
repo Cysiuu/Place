b130656218912d7f654be1708503c8c00bb06a87
Cysiuu <cysiuu021@protonmail.com>
2025-02-20 13:01:49 +0100

    Add new routes

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,24 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/explore', function () {
+    return view('explore');
+});
+
+Route::get('/top-places', function () {
+    return view('top-places');
+});
+
+Route::get('/collections', function () {
+    return view('collections');
+});
+
+Route::get('/near-me', function () {
+    return view('near-me');
+});
+
+
+
