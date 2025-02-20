f296ab0a0adcb6dd14036c7af94005dca9653c4a
Cysiuu <cysiuu021@protonmail.com>
2025-02-20 13:38:23 +0100

    Add nav bar & routes names

--- routes/web.php
+++ routes/web.php
@@ -4,23 +4,22 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
 Route::get('/explore', function () {
     return view('explore');
-});
+})->name('explore');
 
 Route::get('/top-places', function () {
     return view('top-places');
-});
+})->name('top-places');
 
 Route::get('/collections', function () {
     return view('collections');
-});
+})->name('collections');
 
 Route::get('/near-me', function () {
     return view('near-me');
-});
-
+})->name('near-me');
 
 
