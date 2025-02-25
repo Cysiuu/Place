270e26e4328e7c780e3f6bfccfa41196870ebf4c
Krzysztof Kozyra <cysiuu021@protonmail.com>
2025-02-25 14:38:08 +0100

    Test that nav link layouts have correct styling

---
+++ tests/Unit/components/NavLinkTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Unit\components;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\Concerns\InteractsWithViews;
+
+class NavLinkTest extends TestCase
+{
+
+    use InteractsWithViews;
+
+    public function test_active_prop_adds_correct_color_class(): void
+    {
+        $view = $this->blade('<x-nav-link href="/" :active="true">Home</x-nav-link>');
+
+        $view->assertSee('text-orange-500', false);
+        $view->assertSee('font-extrabold', false);
+    }
+
+    public function test_inactive_state_has_default_classes()
+    {
+        $view = $this->blade(
+            '<x-nav-link :active="false">Link Text</x-nav-link>'
+        );
+
+        $view->assertSee('text-gray-700', false);
+        $view->assertDontSee('font-extrabold', false);
+    }
+
+    public function test_classes_are_properly_merged_with_additional_attributes()
+    {
+        $view = $this->blade(
+            '<x-nav-link :active="true" class="custom-class">Link Text</x-nav-link>'
+        );
+
+        $view->assertSee('custom-class', false);
+    }
+
+}
